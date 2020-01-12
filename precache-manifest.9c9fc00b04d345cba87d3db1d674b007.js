810b6f5f8bddf04090c210cba9420ecb440cba15
theExperienced <mizradan@gmail.com>
2020-01-12 13:44:47 +0200

    Updates

---
+++ precache-manifest.9c9fc00b04d345cba87d3db1d674b007.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "325931f2cb15601ddb86ef2627116e66",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "198db0602eec13d49dd6",
+    "url": "/TodoList/static/css/main.e94663aa.chunk.css"
+  },
+  {
+    "revision": "9e46c6bb7c1e8c29cf70",
+    "url": "/TodoList/static/js/2.1a5ab8c5.chunk.js"
+  },
+  {
+    "revision": "198db0602eec13d49dd6",
+    "url": "/TodoList/static/js/main.fe86b10e.chunk.js"
+  },
+  {
+    "revision": "a984db609f4e89573021",
+    "url": "/TodoList/static/js/runtime-main.fddc3055.js"
+  }
+]);
